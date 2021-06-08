d11055a2a0061937c48516017933e59fb6c94269
josymarss <sylvajosymar@gmail.com>
2021-06-08 02:10:32 +0100

    Page add new page is aleredy done, its time to consume data from the database

--- pages/project/newproject.js
+++ pages/project/newproject.js
@@ -1,10 +1,12 @@
 import { useState,useEffect } from 'react'
 import Image from 'next/image'
 
+import styles from './newproject.module.css'
+
 export default function NewProject(){
 
      const [name,setName] = useState('')
-     const [name,setDescription] = useState('')
+     const [description,setDescription] = useState('')
 
      const onAdd = () => {
           e.preventDefault()
@@ -24,27 +26,29 @@ export default function NewProject(){
      }
      
      return(
-          <form onSubmit={onAdd} className='container'>
-               <h1>Add new project</h1>
-               <div className='input'>
-                    <input
-                         type='text'
-                         name='name' 
-                         placeholder='App name' 
-                         onChange={e => setName(e.target.value)}
-                    />
-                    <input
-                         type='text'
-                         name='descritption' 
-                         placeholder='Description' 
-                         onChange={e => setDescription(e.target.value)}
-                    />
+          <form onSubmit={onAdd} className={styles.form}>
+               <div className={styles.container}>
+                    <h1>Add new project</h1>
+                    <div className={styles.input}>
+                         <input
+                              type='text'
+                              name='name' 
+                              placeholder='App name' 
+                              onChange={e => setName(e.target.value)}
+                         />
+                         <input
+                              type='text'
+                              name='descritption' 
+                              placeholder='Description' 
+                              onChange={e => setDescription(e.target.value)}
+                         />
+                    </div>
+                    <div className={styles.image}>
+                         <input type='file' name='file' className={styles.inputFile} onChange={loadImage}/>
+                         <button>Save</button>
+                    </div>
+                   
                </div>
-               <div className='image'>
-                    <input type='file' name='file' onChange={loadImage}/>
-                    <button>Add image</button>
-               </div>
-               <button>Save</button>
           </form>
      );
 }
